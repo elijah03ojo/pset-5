4bd3ee40e5277e6fb5075737eb55bdbafeb03a21
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-12-05 13:04:09 -0500

    Update shapes.js

--- js/shapes.js
+++ js/shapes.js
@@ -1,77 +1,91 @@
-
-// Work now
-
 window.onload = function() {
-  document.getElementById("hello-sample").onclick = sayHelloStaff;
-  document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
-  document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
-  document.getElementById("triangle-sample").onclick = drawTriangleStaff;
-  document.getElementById("smile-sample").onclick = drawFaceStaff;
-  document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
+    document.getElementById("hello-sample").onclick = sayHelloStaff;
+    document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
+    document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
+    document.getElementById("triangle-sample").onclick = drawTriangleStaff;
+    document.getElementById("smile-sample").onclick = drawFaceStaff;
+    document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
 
-  // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-  //
-  // document.getElementById("some-id")   <-- you need to give each button a unique ID
-  //                                          and access it in this manner
-  //
-  // onclick is an event that is fired when you click something (in our case, a button).
-  // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-  // you should set onclick equal to your function names (i.e., sayHello).
-  //
-  // there are six event listeners being added for the staff solutions. you'll have an
-  // equivalent set of six event listeners for your solutions. the first one is done for you.
+    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
+    //
+    // document.getElementById("some-id")   <-- you need to give each button a unique ID
+    //                                          and access it in this manner
+    //
+    // onclick is an event that is fired when you click something (in our case, a button).
+    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
+    // you should set onclick equal to your function names (i.e., sayHello).
+    //
+    // there are six event listeners being added for the staff solutions. you'll have an
+    // equivalent set of six event listeners for your solutions. the first one is done for you.
 
-  this.document.getElementById("hello").onclick = sayHello;
+    document.getElementById("hello").onclick = sayHello;
 }
 
 /*
-* Exercise 1.
-*/
+ * Exercise 1.
+ */
 
 const sayHello = function() {
   const canvas = document.getElementById('student-canvas-1');
   const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = 'rgb(200, 0, 0)';
-  ctx.fillRect(10, 10, 50, 50);
+
+  do {
+    var message = prompt("Message: ")
+    if (message.length > 50) {
+      alert("Your message is too long. Keep it under 50 characters")
+    }
+  } while(message.length > 50)
+
+  ctx.font = "48px sans-serif";
+  ctx.strokeText(message, 30, 70, 994);
+
 };
 
 /*
-* Exercise 2.
-*/
+ * Exercise 2.
+ */
 
 const drawRectangle = function() {
-  // write your exercise 2 code here
+  const canvas = document.getElementById('student-canvas-2');
+  const ctx = canvas.getContext('2d');
+  do {
+    var width = prompt("Width: ")
+    var height = prompt("Height: ")
+    var x = prompt("X: ")
+    var y = prompt("Y: ")
+  } while (width > 1024 || width < 1 || height > 1024 || height < 1)
 };
 
 /*
-* Exercise 3.
-*/
+ * Exercise 3.
+ */
 
 const drawColoredRectangle = function() {
-  // write your exercise 3 code here
+    // write your exercise 3 code here
 };
 
 /*
-* Exercise 4.
-*/
+ * Exercise 4.
+ */
 
 const drawTriangle = function() {
-  // write your exercise 4 code here
+    // write your exercise 4 code here
 };
 
 /*
-* Exercise 5.
-*/
+ * Exercise 5.
+ */
 
 const drawFace = function() {
-  // write your exercise 4 code here
+    // write your exercise 4 code here
 };
 
 /*
-* Exercise 6 (extra credit).
-*/
+ * Exercise 6 (extra credit).
+ */
 
 const drawPyramid = function() {
-  // write your exercise 5 code here
+    // write your exercise 5 code here
 };
